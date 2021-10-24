039d7dc7900d83c0c844a6d80e8f068e7ef7d5f2
KodaiTakano <takano_x_kodai@icloud.com>
2021-10-24 12:00:22 +0900

    candle表示

---
+++ pages/App.tsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import Sample from './sample';
+// import './App.css';
+ 
+// export default function App() {
+//   const style = {
+//     width: "50%",
+//     margin: "0 auto",
+//     marginTop: 150,
+//   };
+//   return (
+//     <div className="App">
+//       <div style={style}>
+//         <Sample />
+//       </div>
+//     </div>
+//   );
+// }
